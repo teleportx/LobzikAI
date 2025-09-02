fa23328811af354a31a9c607ec42f488764888bc
Stepan Khozhempo <stepan@khoz.ru>
2025-09-02 19:14:27 +0300

    feat: lecture_id is uuid

--- service_web/handlers/lecture.py
+++ service_web/handlers/lecture.py
@@ -1,3 +1,5 @@
+import uuid
+
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 from starlette.requests import Request
@@ -10,14 +12,14 @@ templates = Jinja2Templates('templates')
 
 
 @router.get('/{lecture_id}')
-async def handle_lecture_html(request: Request, lecture_id: int):
+async def handle_lecture_html(request: Request, lecture_id: uuid.UUID):
     return templates.TemplateResponse(request=request, name='lecture.html', context={
         'lecture_id': lecture_id,
     })
 
 
 @router.get('/{lecture_id}/data')
-async def handle_lecture_data(request: Request, lecture_id: int):
+async def handle_lecture_data(request: Request, lecture_id: uuid.UUID):
     lecture = (await request.state.db.execute(
         select(db.Lecture.summarized_text)
         .where(db.Lecture.id == lecture_id)
