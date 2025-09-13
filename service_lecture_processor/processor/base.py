246e0b69f5cacc0c2a1033b7a8ac7359e0d15feb
Vitallya <vitaliy.petreev@gmail.com>
2025-09-13 16:19:18 +0300

    Deleted secret + returned environ to BsaeProcessor class

--- service_lecture_processor/processor/base.py
+++ service_lecture_processor/processor/base.py
@@ -1,9 +1,10 @@
+import os
 from abc import ABC, abstractmethod
 
 
 class BaseProcessor(ABC):
     def __init__(self):
-        self.api_key = "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"
+        self.api_key = os.environ["OPENROUTER_KEY"]
         self.url = "https://openrouter.ai/api/v1/chat/completions"
 
         self.headers = {
