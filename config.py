3dd3378c68e106e9b6d1b9b7f0f88dd59ced28a3
Vitallya <vitaliy.petreev@gmail.com>
2025-09-13 23:10:34 +0300

    Added MultiThreadASR

--- config.py
+++ config.py
@@ -14,8 +14,10 @@ bot_token = environ['BOT_TOKEN']
 
 db_pool_max_size = 5
 
+model_cache_dir = environ.get("MODEL_CACHE_DIR", "./cache")
 asr_model = environ.get("ASR_MODEL", "google/gemini-2.5-flash")
 sum_model = environ.get("SUMMARIZATION_MODEL", "gpt-4o-mini")
 mm_model = environ.get("MM_MODEL", "google/gemini-2.5-flash")
+local_asr_vosk_model = environ.get("LOCAL_ASR_VOSK_MODEL", "vosk-model-ru-0.22")
 
 telegram_bot_api_server = environ.get('TELEGRAM_BOT_API_SERVER')
