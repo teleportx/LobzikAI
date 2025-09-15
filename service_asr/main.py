dbfc7bc387003c03d62cb210755d589084910ded
Stepan Khozhempo <stepan@khoz.ru>
2025-09-15 18:25:25 +0300

    logging

--- service_asr/main.py
+++ service_asr/main.py
@@ -43,6 +43,7 @@ async def on_message(message: DeliveredMessage):
     encoded_file = base64.b64encode(file.read()).decode()
 
     result = model(audio_base64=encoded_file)
+    print('OR')
 
     body['asr_result'] = result
     callback_body = json.dumps(body, separators=(',', ':')).encode()
