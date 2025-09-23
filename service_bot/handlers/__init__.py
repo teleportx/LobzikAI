0f390efb1edd60eccb4bbf752f0c39e783fc0839
Stepan Khozhempo <stepan@khoz.ru>
2025-09-23 15:19:41 +0300

    feat: remove teacher from bot

--- service_bot/handlers/__init__.py
+++ service_bot/handlers/__init__.py
@@ -3,7 +3,6 @@ from aiogram import Router
 from . import start
 from . import audio
 from . import inline
-from . import teacher
 from . import lecture
 
 router = Router()
@@ -11,5 +10,4 @@ router = Router()
 router.include_router(start.router)
 router.include_router(audio.router)
 router.include_router(inline.router)
-router.include_router(teacher.router)
 router.include_router(lecture.router)
