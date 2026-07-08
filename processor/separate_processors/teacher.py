db88875d06b3a6ea7a80e3cfce88c64e375050ee
Vitally <vitaliy.petreev@gmail.com>
2026-07-09 02:05:47 +0300

    Fixed import errors

--- processor/separate_processors/teacher.py
+++ processor/separate_processors/teacher.py
@@ -3,7 +3,7 @@ from typing import Any
 from openai import AsyncOpenAI
 
 from processor.base import BaseProcessor
-from schemas import TeacherResponseModel
+from processor.schemas import TeacherResponseModel
 
 import config
 
