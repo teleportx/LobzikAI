2e22a72ee452f4f512ce815a75c6190d98cb7299
Vitally <vitaliy.petreev@gmail.com>
2026-07-09 01:50:36 +0300

    Added separate function for regenerations

--- processor/summarizer_agent/conditions/start_continue_condition.py
+++ processor/summarizer_agent/conditions/start_continue_condition.py
@@ -2,6 +2,6 @@ from processor.summarizer_agent.state import AgentState
 
 
 def start_continue_condition(state: AgentState):
-    if state.messages_history:
+    if state.to_regenerate:
         return "regenerate"
     return "summarize"
--- processor/summarizer_agent/state.py
+++ processor/summarizer_agent/state.py
@@ -6,14 +6,17 @@ from processor.schemas import TestMakerResponseModel
 
 
 class AgentState(BaseModel):
-    messages_history: list[dict[str, Any]] = Field(default_factory=list)
     extracted_text: str
     make_test: bool
-    regenerate_tests: bool
-    custom_instructions: str
-    regeneration_instructions: str
+    custom_instructions: str = Field(default_factory=str)
 
-    ai_response: str = ""
+    to_regenerate: bool = Field(default=False)
+    previous_response: str = Field(default_factory=str)
+    regenerate_tests: bool = Field(default=True)
+    regeneration_instructions: str = Field(default_factory=str)
+    messages_history: list[dict[str, Any]] = Field(default_factory=list)
+
+    ai_response: str = Field(default_factory=str)
     generated_tests: TestMakerResponseModel = Field(default_factory=TestMakerResponseModel)
-    title: str = ""
-    total_cost: Annotated[float, add] = 0
+    title: str = Field(default_factory=str)
+    total_cost: Annotated[float, add] = Field(default=0)
